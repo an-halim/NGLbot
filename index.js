439fa8c1737f0f15c0e43a0385093864e2f69b05
an-halim <halimbla2@gmail.com>
2022-07-03 00:16:37 +0700

    bot ready to use

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import NGL from './NGL.js'
 
-const bot = new NGL('anhalim', { random: true, text: 'hii are you oke?' })
+const bot = new NGL('anhalim', { random: true, text: 'hi are you oke?' })
 bot.sent({count: 10});
