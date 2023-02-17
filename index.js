a521ef193bc6e393356b5bb229a96f3542d6d7d1
an-halim <halimbla2@gmail.com>
2023-02-17 23:01:16 +0700

    update nglink API

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-import NGL from './src/NGL.js'
+import NGL from "./src/NGL.js";
+import readline from "readline";
 
-const bot = new NGL('bsb89706', { random: true, text: 'hi are you oke?' })
-bot.sent({count: 10});
+const userName = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log("\x1b[95mWelcome to NGL bot\x1b[39m");
+userName.question("What is your username? ", (name) => {
+  const bot = new NGL(name, { random: true, text: "hi are you oke?" });
+  bot.sent({ count: 10 });
+  userName.close();
+});
